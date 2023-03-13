6883c8318cde0d335fb16aa9cb9a4e53cd54fef5
Ruchitha Chinta <122854121+ruchitha36@users.noreply.github.com>
2023-03-13 21:14:08 +0530

    Add files via upload

---
+++ 13_display_100_to_110.py
@@ -0,0 +1,15 @@
+# without using any variable
+for i in range(100,111):
+    print(i)
+
+# with the help of a variable
+a=range(100,111)
+for y in a:
+    print(y)
+
+""" with the help of while """
+m=int(input("Enter the value of m:"))
+n=int(input("Enter the value of n:"))
+while(m<=n):
+    print(m)
+    m+=1
